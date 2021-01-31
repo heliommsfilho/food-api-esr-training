4dac5c65ac563039bcbfdb0affed1f2034e7e859
Hélio Márcio Filho <heliommsfilho@gmail.com>
2021-01-31 13:11:25 +0000

    5.7. Usando as keywords para definir critérios de query methods

--- src/main/java/com/github/heliommsfilho/foodapi/domain/repository/CozinhaRepository.java
+++ src/main/java/com/github/heliommsfilho/foodapi/domain/repository/CozinhaRepository.java
@@ -4,7 +4,12 @@ import com.github.heliommsfilho.foodapi.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
+    List<Cozinha> findTodasByNomeContaining(String nome);
+    Optional<Cozinha> findByNome(String nome);
 }
