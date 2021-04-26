fb325f13bd9a240e59fcfc39ef0c41a42d4ca2be
Hélio Márcio Filho <heliommsfilho@gmail.com>
2021-04-26 12:03:18 +0100

    8.24. Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/github/heliommsfilho/foodapi/controller/RestauranteController.java
+++ src/main/java/com/github/heliommsfilho/foodapi/controller/RestauranteController.java
@@ -1,14 +1,20 @@
 package com.github.heliommsfilho.foodapi.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.github.heliommsfilho.foodapi.domain.exception.CozinhaNaoEncontradaException;
 import com.github.heliommsfilho.foodapi.domain.exception.NegocioException;
 import com.github.heliommsfilho.foodapi.domain.model.Restaurante;
 import com.github.heliommsfilho.foodapi.domain.repository.RestauranteRepository;
 import com.github.heliommsfilho.foodapi.domain.service.CadastroRestauranteService;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpInputMessage;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.List;
@@ -87,25 +94,33 @@ public class RestauranteController {
     }
 
     @PatchMapping("/{restauranteId}")
-    public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+    public Restaurante atualizarParcial(HttpServletRequest request, @PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
         Restaurante restauranteAtual = cadastroRestauranteService.buscarOuFalhar(restauranteId);
-        merge(campos, restauranteAtual);
+        merge(request, campos, restauranteAtual);
         return atualizar(restauranteId, restauranteAtual);
     }
 
-    private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+    private void merge(HttpServletRequest request, Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
         ObjectMapper mapper = new ObjectMapper();
-        Restaurante restauranteOrigem = mapper.convertValue(camposOrigem, Restaurante.class);
+        mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
-        camposOrigem.forEach((propriedade, valor) -> {
-            Optional<Field> field = Optional.ofNullable(ReflectionUtils.findField(Restaurante.class, propriedade));
+        try {
+            Restaurante restauranteOrigem = mapper.convertValue(camposOrigem, Restaurante.class);
 
-            if (field.isPresent()) {
-                field.get().setAccessible(true);
+            camposOrigem.forEach((propriedade, valor) -> {
+                Optional<Field> field = Optional.ofNullable(ReflectionUtils.findField(Restaurante.class, propriedade));
 
-                Object novoValor = ReflectionUtils.getField(field.get(), restauranteOrigem);
-                ReflectionUtils.setField(field.get(), restauranteDestino, novoValor);
-            }
-        });
+                if (field.isPresent()) {
+                    field.get().setAccessible(true);
+
+                    Object novoValor = ReflectionUtils.getField(field.get(), restauranteOrigem);
+                    ReflectionUtils.setField(field.get(), restauranteDestino, novoValor);
+                }
+            });
+        } catch (IllegalArgumentException e) {
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, new ServletServerHttpRequest(request));
+        }
     }
 }
