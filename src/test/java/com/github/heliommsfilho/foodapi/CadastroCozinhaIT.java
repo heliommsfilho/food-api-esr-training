f936b0c9250a4b33a4c9caa0d5a7a47aabe9fd91
Hélio Márcio Filho <heliommsfilho@gmail.com>
2021-09-25 17:04:07 +0100

    10.10. Criando um método para fazer setup dos testes

--- src/test/java/com/github/heliommsfilho/foodapi/CadastroCozinhaIT.java
+++ src/test/java/com/github/heliommsfilho/foodapi/CadastroCozinhaIT.java
@@ -4,6 +4,7 @@ package com.github.heliommsfilho.foodapi;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -15,13 +16,17 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
     
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
+    
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         
         RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
             .when().get()
             .then().statusCode(HttpStatus.OK.value());
@@ -29,11 +34,8 @@ public class CadastroCozinhaIT {
     
     @Test
     public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         
         RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
             .when().get()
             .then()
