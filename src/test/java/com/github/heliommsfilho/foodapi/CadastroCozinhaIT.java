f0316278126bd833c5194caceade519ab65d7623
Hélio Márcio Filho <heliommsfilho@gmail.com>
2021-09-25 17:00:57 +0100

    10.9. Validando o corpo da resposta HTTP

--- src/test/java/com/github/heliommsfilho/foodapi/CadastroCozinhaIT.java
+++ src/test/java/com/github/heliommsfilho/foodapi/CadastroCozinhaIT.java
@@ -3,6 +3,7 @@ package com.github.heliommsfilho.foodapi;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -24,5 +25,19 @@ public class CadastroCozinhaIT {
                 .accept(ContentType.JSON)
             .when().get()
             .then().statusCode(HttpStatus.OK.value());
+    }
+    
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        
+        RestAssured.given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+            .when().get()
+            .then()
+                .body("", Matchers.hasSize(4))
+                .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
     }
 }
