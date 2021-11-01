0a28736e55a97abdff3750d027e2df2f6c89c02f
Hélio Márcio Filho <heliommsfilho@gmail.com>
2021-11-01 18:44:25 +0000

    12.11. Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/github/heliommsfilho/foodapi/domain/repository/UsuarioRepository.java
+++ src/main/java/com/github/heliommsfilho/foodapi/domain/repository/UsuarioRepository.java
@@ -4,7 +4,10 @@ import com.github.heliommsfilho.foodapi.domain.model.Usuario;
 import com.github.heliommsfilho.foodapi.infrastructure.repository.CustomJPARepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends CustomJPARepository<Usuario, Long> {
-
+    
+    Optional<Usuario> findByEmail(final String email);
 }
